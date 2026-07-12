d8827ded958e59e1e6127ac02c43f59271095109
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:02:58 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "log_analytics_cluster_customer_managed_keys_id" {
+  description = "Map of id values across all log_analytics_cluster_customer_managed_keys, keyed the same as var.log_analytics_cluster_customer_managed_keys"
+  value       = { for k, v in azurerm_log_analytics_cluster_customer_managed_key.log_analytics_cluster_customer_managed_keys : k => v.id }
+}
 output "log_analytics_cluster_customer_managed_keys_key_vault_key_id" {
   description = "Map of key_vault_key_id values across all log_analytics_cluster_customer_managed_keys, keyed the same as var.log_analytics_cluster_customer_managed_keys"
   value       = { for k, v in azurerm_log_analytics_cluster_customer_managed_key.log_analytics_cluster_customer_managed_keys : k => v.key_vault_key_id }
