b8e976a2eb39235a401ac6645ecff4deb55a738b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:19 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "log_analytics_cluster_customer_managed_keys" {
-  description = "All log_analytics_cluster_customer_managed_key resources"
-  value       = azurerm_log_analytics_cluster_customer_managed_key.log_analytics_cluster_customer_managed_keys
-}
 output "log_analytics_cluster_customer_managed_keys_key_vault_key_id" {
-  description = "List of key_vault_key_id values across all log_analytics_cluster_customer_managed_keys"
-  value       = [for k, v in azurerm_log_analytics_cluster_customer_managed_key.log_analytics_cluster_customer_managed_keys : v.key_vault_key_id]
+  description = "Map of key_vault_key_id values across all log_analytics_cluster_customer_managed_keys, keyed the same as var.log_analytics_cluster_customer_managed_keys"
+  value       = { for k, v in azurerm_log_analytics_cluster_customer_managed_key.log_analytics_cluster_customer_managed_keys : k => v.key_vault_key_id }
 }
 output "log_analytics_cluster_customer_managed_keys_log_analytics_cluster_id" {
-  description = "List of log_analytics_cluster_id values across all log_analytics_cluster_customer_managed_keys"
-  value       = [for k, v in azurerm_log_analytics_cluster_customer_managed_key.log_analytics_cluster_customer_managed_keys : v.log_analytics_cluster_id]
+  description = "Map of log_analytics_cluster_id values across all log_analytics_cluster_customer_managed_keys, keyed the same as var.log_analytics_cluster_customer_managed_keys"
+  value       = { for k, v in azurerm_log_analytics_cluster_customer_managed_key.log_analytics_cluster_customer_managed_keys : k => v.log_analytics_cluster_id }
 }
 
