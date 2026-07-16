6c9e42a8146db8a2c0fa0c4d2b140e93bbbdf092
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:48:49 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "log_analytics_cluster_customer_managed_keys_id" {
   description = "Map of id values across all log_analytics_cluster_customer_managed_keys, keyed the same as var.log_analytics_cluster_customer_managed_keys"
-  value       = { for k, v in azurerm_log_analytics_cluster_customer_managed_key.log_analytics_cluster_customer_managed_keys : k => v.id }
+  value       = { for k, v in azurerm_log_analytics_cluster_customer_managed_key.log_analytics_cluster_customer_managed_keys : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "log_analytics_cluster_customer_managed_keys_key_vault_key_id" {
   description = "Map of key_vault_key_id values across all log_analytics_cluster_customer_managed_keys, keyed the same as var.log_analytics_cluster_customer_managed_keys"
-  value       = { for k, v in azurerm_log_analytics_cluster_customer_managed_key.log_analytics_cluster_customer_managed_keys : k => v.key_vault_key_id }
+  value       = { for k, v in azurerm_log_analytics_cluster_customer_managed_key.log_analytics_cluster_customer_managed_keys : k => v.key_vault_key_id if v.key_vault_key_id != null && length(v.key_vault_key_id) > 0 }
 }
 output "log_analytics_cluster_customer_managed_keys_log_analytics_cluster_id" {
   description = "Map of log_analytics_cluster_id values across all log_analytics_cluster_customer_managed_keys, keyed the same as var.log_analytics_cluster_customer_managed_keys"
-  value       = { for k, v in azurerm_log_analytics_cluster_customer_managed_key.log_analytics_cluster_customer_managed_keys : k => v.log_analytics_cluster_id }
+  value       = { for k, v in azurerm_log_analytics_cluster_customer_managed_key.log_analytics_cluster_customer_managed_keys : k => v.log_analytics_cluster_id if v.log_analytics_cluster_id != null && length(v.log_analytics_cluster_id) > 0 }
 }
 
